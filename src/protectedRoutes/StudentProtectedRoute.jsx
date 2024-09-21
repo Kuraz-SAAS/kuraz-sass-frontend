7c1aca06b9d8038fa4f67cd4b537ec0b4051364c
y123an <yohannesaregahabte@gmail.com>
2024-09-21 21:59:03 +0300

    feat: added login redirectioin

--- src/protectedRoutes/StudentProtectedRoute.jsx
+++ src/protectedRoutes/StudentProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { useSiteStore } from "../context/siteStore";
 
 const StudentProtectedRoute = () => {
   let user = useSiteStore((store) => store.user);
-  return user && user.user_type.toLowerCase() === "student" ? (
+  return user && user?.user_type?.toLowerCase() === "student" ? (
     <Outlet />
   ) : (
     <Navigate to={"/login"} />
