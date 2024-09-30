f99c63bcb5eaa88234b151d0f4ad6fa6360f7781
y123an <yohannesaregahabte@gmail.com>
2024-09-30 15:53:49 +0300

    fixed the subject error

--- src/protectedRoutes/SchoolProtectedRoute.jsx
+++ src/protectedRoutes/SchoolProtectedRoute.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 import { Navigate, Outlet } from "react-router-dom";
 import { useSiteStore } from "../context/siteStore";
+import Cookies from "js-cookie";
 
 const SchoolProtectedRoute = () => {
+  const cookies = document.cookie;
+  console.log(cookies);
   let user = useSiteStore((store) => store.user);
   return user && user?.user_type?.toLowerCase() === "school" ? (
     <Outlet />
